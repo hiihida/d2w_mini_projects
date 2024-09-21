6e5d9daf59c7d17ab65e0fd0b214246365d31695
hiihida <hidxie@gmail.com>
2024-09-21 19:04:46 +0800

    Did exercise 1, made minor colour changes on the html files

--- mp_sort/app/static/library.py
+++ mp_sort/app/static/library.py
@@ -2,27 +2,54 @@ from org.transcrypt.stubs.browser import *
 import random
 
 def gen_random_int(number, seed):
-	pass
+
+	random.seed(seed)
+	result = random.randint(-10, 10)
+	#print(result)
+	return result
 
 def generate():
 	number = 10
 	seed = 200
+	array = []
 
 	# call gen_random_int() with the given number and seed
 	# store it to the variable array
-	pass
+	for i in range(number):
+		array.append(gen_random_int(number, seed))
+	print(array)
 
-	array = None
 	# convert the items into one single string 
 	# the number should be separated by a comma
 	# and a full stop should end the string.
-	pass
-
-	array_str = None
-
+	array_str = ls_to_str(array)
+	
 	# This line is to placed the string into the HTML
 	# under div section with the id called "generate"	
 	document.getElementById("generate").innerHTML = array_str
+
+
+def ls_to_str(array):
+	array_str = ""
+
+	for i in range(len(array)):
+		array_str += str(array[i])
+		if i == len(array)-1:
+			array_str += "."
+		else:
+			array_str += ", "
+	return array_str
+
+
+def insertion_sort(array):
+	n = len(array)
+	
+	for border_index in range(1, n):
+		small_index = border_index
+		while small_index > 0 and array[small_index-1] > array[small_index]:
+			array[small_index-1], array[small_index] = array[small_index], array[small_index-1]
+			small_index -= 1
+	return array
 
 
 def sortnumber1():
@@ -35,11 +62,23 @@ def sortnumber1():
 		- call your sort function, either bubble sort or insertion sort
 		- create a string of the sorted numbers and store it in array_str
 	'''
-	pass
+	ls_str = document.getElementById("generate").innerHTML
+	ls_int = []
 
-	array_str = None
+	ls_str = ls_str.replace(".", ", ") # replace '.' at end of str
+	ls_str = ls_str.split(", ") # separate by ", "
+	ls_str = ls_str[:-1] # remove empty item in last index
+	#print(ls_str)
+
+	for i in ls_str:
+		ls_int.append(int(i))
+	#print(ls_int)
+
+	sorted_array = insertion_sort(ls_int)
+	array_str = ls_to_str(sorted_array)
 	
 	document.getElementById("sorted").innerHTML = array_str
+
 
 def sortnumber2():
 	'''	This function is used in Exercise 2.
